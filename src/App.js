62ea2e4da05b6f81ee137b3e833964df14a957d8
System Administrator <root@MyMacBookAir.local>
2021-06-06 15:31:17 -0700

    first commit1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload again.
+          Edit <code>src/App.js</code> and save to reload again. Of course this is a new tested version.
         </p>
         <a
           className="App-link"
